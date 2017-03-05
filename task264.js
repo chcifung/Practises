60ed0655cab9bf3fc3ed1187e1613c5538784f3d
chcifung <chcifung@gmail.com>
2017-03-05 20:40:30 +0800

    Add files via upload

--- task264.js
+++ task264.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 	//get the free code camp stream info and status api call
+	var following = [];
 	var url = "https://wind-bow.gomix.me/twitch-api/streams/freecodecamp";
 	$.getJSON(url,function(data1){
 		if(data1.stream === null){
@@ -8,4 +9,47 @@ $(document).ready(function(){
 			$("#fccStatus").html("freecodecamp is currentlt online");
 		}
 	});
+
+	var followerURL = "https://wind-bow.gomix.me/twitch-api/users/freecodecamp/follows/channels/";
+	$.getJSON(followerURL,function(data2){
+		for(var i = 0;i<data2.follows.length;i++){
+			var displayName = data2.follows[i].channel.display_name;
+			following.push(displayName);
+		}
+
+		following.push("comster404");
+		following.push("brunofin");
+		following.push("ESL_SC2");
+
+
+		for (var i = 0; i <following.length; i++) {
+			var url2 = 'https://wind-bow.gomix.me/twitch-api/streams/' + following[i] + '/?callback=?';
+			console.log(url2);
+			$.getJSON(url2,function(data3){
+				var logo;
+				var status;
+				var name;
+
+				if(data3.stream===null){
+					logo = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRzFnuxU6ipiMZ-ywenUSoTaNsdiLc8dIaatjVuNRZAD5KvAJto-owFgaln";
+					name = data3.message;
+					status = data3.error;
+					
+					$("#followerInfo").prepend(
+						"<div class ='row'><div class = 'col-md-4'>" + "<img src='" + logo +"'>"
+						+"</div>" + "<div class = 'col-md-4'>" +name +"</div>"+ "<div class = 'col-md-4'>"  +status +"</div></div>"
+						);
+				}
+			});
+		}
+
+		for (var i = following.length - 1; i >= 0; i--) {
+			var onlineURL = 'https://wind-bow.gomix.me/twitch-api/streams/' + following[i] +
+			$.getJSON(onlineURL,function(data4){
+				if(data4.status != null&& data4.error!=true){
+
+				}
+			})
+		}
+	})
 })
