414979e45d31938c87f9a68c20efbd338b80e7bf
chcifung <chcifung@gmail.com>
2017-03-12 18:14:45 +0800

    Add files via upload

--- task276.js
+++ task276.js
@@ -7,7 +7,14 @@ function spinalCase(str) {
   str = str.replace(/_/g,' ').replace(/([A-Z])/g," $1").replace(/^\s/,'').replace(/\s+/g,"-").toLowerCase();
   
   return str;
-  
+  /*
+  1.- to ' ';
+  2.A-Z to add ' ';
+  3.remove the first ' ';
+  4.' ' to '-';
+  5. to lower case;
+  6. old - dont need to change;
+  */
 }
 
 spinalCase('This Is Spinal Tap');
