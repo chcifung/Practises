1b0b8905c1fd623450d2f8c3420cf4507209c830
chcifung <chcifung@gmail.com>
2017-03-13 20:14:18 +0800

    Add files via upload

--- task277.js
+++ task277.js
@@ -1,15 +1,21 @@
+
 function sumFibs(num) {
   var a,b,sum;
   a=b=1;
-  if(num==1){
-    return 1;
-  }
-  for(var i=1;i<num;i++){
+  var sums = [1,1];
+  for(var i=2;i<num;i++){
     sum = a +b;
     a = b;
     b=sum;
+    if(b%2==1&&b<=num){
+      sums.push(b);
+    }
+  }//arrange all the odd number and not bigger than the num
+  var result=0;
+  for(var j=0;j<sums.length;j++){
+     result = result +sums[j];
   }
-  return sum;
+  return result;// get the sum of the array
 }
 
 sumFibs(4);
