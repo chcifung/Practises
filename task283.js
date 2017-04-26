2369462ef3deb5a1dfeabb0178d9a88d4d99990e
蒋怡 <chcifung@gmail.com>
2017-04-26 21:54:51 +0800

    Add files via upload

---
+++ task283.js
@@ -0,0 +1,12 @@
+function binaryAgent(str) {
+  var a =[];
+  a.push(str.split(" "));
+  for(var i = 0;i<a.length;i++){
+    var b=[];
+    b.push(String.fromCodePoint(a[i]));
+    b.join("");
+  }
+  return b;
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
