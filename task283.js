1782e782438301cb7e4eb7ac5ac1d89983253b21
蒋怡 <chcifung@gmail.com>
2017-05-02 20:17:35 +0800

    Add files via upload

--- task283.js
+++ task283.js
@@ -1,12 +1,11 @@
 function binaryAgent(str) {
-  var a =[];
-  a.push(str.split(" ").toString(16));
-  for(var i = 0;i<a.length;i++){
-    var b=[];
-    b.push(String.fromCodePoint(a[i]));
-    b.join("");
-  }
-  return 0;
+	var a=str.split(" ");
+	var b = [];
+  for(var i=0;i<a.length;i++){
+	b.push(parseInt(a[i],2).toString(16));
+	var c = String.fromCharCode(b[i]);
+	console.log(c);
+  }  
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
