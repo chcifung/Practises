97919d95e1511b48dbbbe383e9dc3a20b4db55d8
蒋怡 <chcifung@gmail.com>
2017-05-02 20:26:44 +0800

    Binary Agents
    Return an English translated sentence of the passed binary string.
    
    The binary string will be space separated.
    
    ## binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111") should return "Aren't bonfires fun!?"
    binaryAgent("01001001 00100000 01101100 01101111 01110110 01100101 00100000 01000110 01110010 01100101 01100101 01000011 01101111 01100100 01100101 01000011 01100001 01101101 01110000 00100001") should return "I love FreeCodeCamp!"

--- task283.js
+++ task283.js
@@ -1,11 +1,12 @@
 function binaryAgent(str) {
 	var a=str.split(" ");
 	var b = [];
+	var c = [];
   for(var i=0;i<a.length;i++){
-	b.push(parseInt(a[i],2).toString(16));
-	var c = String.fromCharCode(b[i]);
-	console.log(c);
+	b.push(parseInt(a[i],2).toString(10));
+	c.push(String.fromCharCode(b[i]));
   }  
+  return c.join('');
 }
 
 binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
