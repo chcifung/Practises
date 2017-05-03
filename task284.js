a3e497cd3840b443e3b0c05937284dc08a5a2e67
蒋怡 <chcifung@gmail.com>
2017-05-03 20:17:39 +0800

    Everything Be True
    Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
    
    ## truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return true.
    truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex") should return false.

--- task284.js
+++ task284.js
@@ -2,20 +2,17 @@ function truthCheck(collection, pre) {
   // Is everyone being true?
   var sum = 0;
   for(var i = 0;i<collection.length;i++){
-    if(collection[i].hasOwnProperty(pre)){
-      sum =sum +1;
-      
-    }else{
-      sum = 1;
+    
+    if(collection[i][pre]){
+        sum +=1;
     }
-  }
-  
-  if(sum == collection.length-1){
-    return true;
-  }else{
-    return sum;
-  }
-  
+  } 
+
+  if(sum == collection.length){
+        return true;
+    }else{
+        return false;
+    }
 }
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
