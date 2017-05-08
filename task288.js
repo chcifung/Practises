59d89ccb55afc96bcdcc4a4a7dcf3177a327b495
蒋怡 <chcifung@gmail.com>
2017-05-08 21:35:20 +0800

    Symmetric Difference
    Create a function that takes two or more arrays and returns an array of the symmetric difference (△ or ⊕) of the provided arrays.
    
    Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, but not in both (A △ B = C = {1, 4}). For every additional symmetric difference you take (say on a set D = {2, 3}), you should get the set with elements which are in either of the two the sets but not both (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}).
    
    Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
    
    三步走：
    1.先把传参放入一个数组中，
    2.把两个参数的数组求出差分；
    3.用reduce函数回调多个数组。

--- task288.js
+++ task288.js
@@ -1,32 +1,28 @@
-function sym(args/* pass one or more arrays here */) {
-    var ans = [], cnts = {}, currentMap;
-
-    //count all items in the array
-    for (var i = 0; i < arguments.length; i++){
-        currentMap = {};
-        arguments[i].forEach(function(item) {
-            // if we haven't already counted this item in this array
-            if (!currentMap.hasOwnProperty(item)) {
-                if (cnts.hasOwnProperty(item)) {
-                    // increase cnt
-                    ++cnts[item].cnt;
-                } else {
-                    // initalize cnt and value
-                    cnts[item] = {cnt: 1, val: item};
-                }
-            }
-            // keep track of whethere we've already counted this item in this array
-            currentMap[item] = true;
-        });
-    }
-    // output all items that have a cnt of 1
-    for (var item in cnts) {
-        if (cnts.hasOwnProperty(item) && cnts[item].cnt === 1) {
-            ans.push(cnts[item].val);
-        }
-    }
-
-    return ans;
+function sym() {
+   var args = [];
+  /*把传参放入一数组中*/
+  for(var i=0;i<arguments.length;i++){
+    args.push(arguments[i]);
+  }
+  /*symDif函数找出两个数组的差分数组*/
+  function symDif(arr1,arr2){
+    var result = [];
+    arr1.forEach(function(item){
+      if(arr2.indexOf(item)<0&&result.indexOf(item)<0){
+        result.push(item);
+      }
+    });
+    arr2.forEach(function(item){
+      if(arr1.indexOf(item)<0&&result.indexOf(item)<0){
+        result.push(item);
+      }
+    });
+    return result;
+    
+  }
+  /*reduce 为数组中的每一个元素依次执行回调函数*/
+  return args.reduce(symDif);
 }
+
 
 sym([1, 2, 3], [5, 2, 1, 4]);
