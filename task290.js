282bdf05d9e0ea6d8c515fe9c620003db9b7bab8
蒋怡 <chcifung@gmail.com>
2017-05-12 21:28:27 +0800

    Add files via upload

---
+++ task290.js
@@ -0,0 +1,30 @@
+function updateInventory(arr1, arr2) {
+    // All inventory must be accounted for or you're fired!
+  var a1 = [];
+  var a2 = [];
+  for(var i=0;i<arr1.length;i++){
+    a1.push(arr1[i][1]);
+  }
+  for(var j=0;j<arr2.length;j++){
+    a2.push(arr2[j][1]);
+  }
+  console.log(a1,a2);
+    return arr1;
+}
+
+// Example inventory lists
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
