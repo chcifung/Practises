884ae3660b876d1cc0e721f8560b65a190207bb7
蒋怡 <chcifung@gmail.com>
2017-05-14 20:26:05 +0800

    No repeats please
    Return the number of total permutations of the provided string that don't have repeated consecutive letters. Assume that all characters in the provided string are each unique.
    
    For example, aab should return 2 because it has 6 total permutations (aab, aab, aba, aba, baa, baa), but only 2 of them (aba and aba) don't have the same letter (in this case a) repeating.

--- task291.js
+++ task291.js
@@ -2,13 +2,40 @@ function permAlone(str) {
   var arr = str.split("");
   var prearr = [];
   var begin = 0;
-  var reg = /(.){1,}/g;
+  var reg = /(.)\1+/g;
   if(str.match(reg)!==null&&str.match(reg)[0]===str){//如果 regexp 具有标志 g，则 match() 方法将执行全局检索，找到 stringObject 中的所有匹配子字符串。
   	//若没有找到任何匹配的子串，则返回 null。如果找到了一个或多个匹配子串，则返回一个数组。
   	//不过全局匹配返回的数组的内容与前者大不相同，它的数组元素中存放的是 stringObject 中所有的匹配子串，而且也没有 index 属性或 input 属性。
   	return 0;
   }
-  return str;
+
+  //swap function
+  function swap(a1,a2){
+  	var temp = arr[a1];
+  	arr[a1] = arr[a2];
+  	arr[a2] = temp;
+  }
+
+  function premall(arr,begin){
+  	if(begin==arr.length-1){
+  		prearr[prearr.length]=arr.join("");
+  		return;
+  	}
+
+  	for(var i =0;i<arr.length-begin;i++){
+  		swap(begin,begin+i);
+  		premall(arr,begin+1);
+  		swap(begin,begin+i);
+  	}
+  }
+
+  premall(arr,begin);
+
+  return prearr.filter(function(val){
+  	return !val.match(reg);
+  }).length;
+
+
 }
 
 permAlone('aab');
