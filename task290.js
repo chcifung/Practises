178141d89bb182c7ece09cb580cdc8dd0d60888f
蒋怡 <chcifung@gmail.com>
2017-05-14 14:04:52 +0800

    Add files via upload

--- task290.js
+++ task290.js
@@ -2,11 +2,25 @@
 function updateInventory(arr1, arr2) {
     // All inventory must be accounted for or you're fired!
   if(arr1.length!==0&&arr2.length!==0){
+    
+    var a1 = [];
+    for(var k=0;k<arr1.length;k++){
+      a1.push(arr1[k][1]);
+    }
+    
+    for(l=0;l<arr2.length;l++){
+      if(a1.indexOf(arr2[l][1])<0){
+        arr1.push(arr2[l]);
+      }
+    }
+    
     for(var i=0;i<arr1.length;i++){
+      
      for(var j=0;j<arr2.length;j++){
        if(arr1[i][1]==arr2[j][1]&&arr1[i][0]!=arr2[j][0]){
         arr1.splice(i,1,arr2[j]);
-       }         
+       }
+       
      }
      
    }
@@ -34,3 +48,4 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
+
