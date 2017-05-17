5531c144871e08170f18c2b4b6c4562246b1f15e
蒋怡 <chcifung@gmail.com>
2017-05-17 21:48:55 +0800

    Pairwise
    Given an array arr, find element pairs whose sum equal the second argument arg and return the sum of their indices.
    
    If multiple pairs are possible that have the same numeric elements but different indices, return the smallest sum of indices. Once an element has been used, it cannot be reused to pair with another.
    
    For example pairwise([7, 9, 11, 13, 15], 20) returns 6. The pairs that sum to 20 are [7, 13] and [9, 11]. We can then write out the array with their indices and values.
    
    Index	0	1	2	3	4
    Value	7	9	11	13	15
    Below we'll take their corresponding indices and add them.
    
    7 + 13 = 20 → Indices 0 + 3 = 3
    9 + 11 = 20 → Indices 1 + 2 = 3
    3 + 3 = 6 → Return 6

--- task294.js
+++ task294.js
@@ -1,10 +1,16 @@
 function pairwise(arr, arg) {
-
-  var sum = arr.reduce(function(pre,cur,index,arr){
-  	if(pre+cur == arg){
-  		sum +=index;
-  	}
-  });
-
+  var sum=0;
+  for(var i=0;i<arr.length;i++){
+    for(var j=i+1;j<arr.length;j++){
+      if(arr[i]+arr[j]==arg){
+        sum +=i+j;
+        arr[i] += ",";//type change 隐式类型转换
+        arr[j] += ",";
+      }
+    }
+  }
   return sum;
 }
+
+
+pairwise([1,4,2,3,0,5], 7);
