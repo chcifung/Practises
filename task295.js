218386ba605a61aa9dd503eb0797a3c5581b5083
蒋怡 <chcifung@gmail.com>
2017-05-19 20:30:40 +0800

    Add files via upload

--- task295.js
+++ task295.js
@@ -1,3 +1,28 @@
-document.ready(function function_name(argument) {
-	// body...
-})
+//find a element in the offer array
+
+function findArr(a,c){
+	for(var b=0;b<a.length;b+=){
+		if(a[b]==c){
+			return true;
+		}
+	}
+	return false;
+}
+
+//find the f class in d;
+
+function getClass(d,f){
+	if(document.getElementsByClassName){
+		return d.getElementsByClassName(f);
+	}else{
+		var a = [];
+		var e =document.getElementsByTagName("*");
+		for(var c = 0;c<e.length;c++){
+			var b = e[c].className.split(" ");
+			if(findArr(b,f)){
+				a.push(e[c]);
+			}
+		}
+		return a;
+	}
+}
