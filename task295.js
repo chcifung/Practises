aab91c915e191eef7c3d7faa236c7a0cbfb47e4a
蒋怡 <chcifung@gmail.com>
2017-05-19 21:40:51 +0800

    Add files via upload

--- task295.js
+++ task295.js
@@ -1,7 +1,7 @@
 //find a element in the offer array
 
 function findArr(a,c){
-	for(var b=0;b<a.length;b+=){
+	for(var b=0;b<a.length;b++){
 		if(a[b]==c){
 			return true;
 		}
@@ -24,5 +24,67 @@ function getClass(d,f){
 			}
 		}
 		return a;
+	}
+}
+
+window.onload = function(){
+	var aNum = getClass(document,"num");
+	var oText = document.getElementById("text");
+	var aPer = getClass(document,"oper");
+	var oPer = document.getElementById("per");
+	var oText1 = document.getElementById("text1");
+	var oDeng = getClass(document,"deng")[0];
+	var oSq = getClass(document,"sq")[0];
+	var oRec = getClass(document,"rec")[0];
+	var oZheng = getClass(document,"zheng")[0];
+	var oOn = getClass(document,"on")[0];
+	var oOff = getClass(document,"off")[0];
+	var oClea = getClass(document,"clea")[0];
+	var bOnOrOffClick = false;
+
+//click events for number elements
+	function fnNum(a){
+		var bClear = false;
+		oText.value = "0";
+		for(var i=0;i<aNum.length;i++){
+			aNum[i].onclick = function(){
+
+				//check the switch
+				if(!bOnOrOffClick) return;
+
+				//check the clear 
+				if(bClear) {
+					bClear = false;
+				}
+
+				//check the dot exist
+				if(oText.value.indexOf(".")!=-1){
+					if(this.innerHTML =="."){
+						return;
+					}
+				}
+
+				//check the operator and input value exist;
+				//input a number and a operator
+				if(oPer.value&&oText.value&&oText1.value ==""){
+					oText1.value = oText.value;
+					oText.value = "";
+				}
+
+				var re = /^0\.{1}\d+$/;
+				var re1 = /^([0]\d+)$/;
+
+				//input to display
+				oText.value +=this.innerHTML;
+
+				if(re.test(oText.value)){
+					return;
+				}
+
+				if(re1.test(oText.value)){
+					oText.value = this.innerHTML;
+				}
+			}
+		}
 	}
 }
