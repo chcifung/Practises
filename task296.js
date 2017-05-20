5ac5d0b22163d705c3ec9661957a98f2f2f85166
蒋怡 <chcifung@gmail.com>
2017-05-20 15:53:55 +0800

    Add files via upload

--- task296.js
+++ task296.js
@@ -2,3 +2,29 @@ var fresh = document.getElementsByClassName("fresh")[0];
 var substract  = document.getElementById("substract");
 var add = document.getElementById("add");
 var period = document.getElementById("period");
+
+add.onclick = function(){
+	var a = Number(period.innerHTML);
+	a +=1;
+	period.innerHTML = a;
+}
+
+substract.onclick = function(){
+
+	var a = Number(period.innerHTML);
+	if(a>0){
+		a -=1;
+		period.innerHTML = a;
+	}else{
+		alert("The during time is wrong!");
+	}		
+}
+
+var switchs = false;
+
+fresh.onclick = function(){
+	var a = new Date();
+	var b = a.getTime();
+	var c = b+(Number(period.innerHTML))*60000;
+	console.log(b,c);
+}
