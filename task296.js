71d7da5183dee57d1cb1e30d648cc588c4f4b4d0
蒋怡 <chcifung@gmail.com>
2017-05-20 15:19:54 +0800

    Add files via upload

---
+++ task296.js
@@ -0,0 +1,4 @@
+var fresh = document.getElementsByClassName("fresh")[0];
+var substract  = document.getElementById("substract");
+var add = document.getElementById("add");
+var period = document.getElementById("period");
