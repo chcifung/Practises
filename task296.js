51bcb9aaa333286ad80647d4681b102866bebb1f
蒋怡 <chcifung@gmail.com>
2017-05-22 21:44:35 +0800

    Add files via upload

--- task296.js
+++ task296.js
@@ -18,28 +18,36 @@ substract.onclick = function(){
 	}		
 }
 
+
+
 var switchs = false;
-
-var periodtime = a*60;
-var now = new Date();
-var end = now.getTime() + periodtime*1000;
-
-function showTime(){
-
-	var nows = new Date();	
-	//var m = parseInt(lefttime/60%60);
-	//var s = parseInt(lefttime%60);
-	var m = parseInt((end - nows)/1000/60%60);
-	var s = parseInt((end - nows)/1000%60);
-	var timer = document.getElementById("timer");
-	timer.innerHTML = m +":" +s;
-	setTimeout(showTime,500);
-}
 
 fresh.onclick = function(){
 	//get the left seconds
+
+
+	var now = new Date();
+	var end = now.getTime() + a*60*1000;
+	function showTime(){
+
+		var nows = new Date();	
+		//var m = parseInt(lefttime/60%60);
+		//var s = parseInt(lefttime%60);
+		var m = parseInt((end - nows)/1000/60%60);
+		var s = parseInt((end - nows)/1000%60);
+		var timer = document.getElementById("timer");
+		timer.innerHTML = m +":" +s;
+		setTimeout(showTime,500);
+	}
 	
-	showTime();
+	if(!switchs){
+		showTime();
+		switchs = true;
+		console.log(1);
+	}else{
+		setInterval(showTime,1000);
+	}
+	
 	
 }
 
