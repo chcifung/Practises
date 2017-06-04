ad3e5a750996c5aab948068fc5f31b2ff85148ec
蒋怡 <chcifung@gmail.com>
2017-06-04 21:12:19 +0800

    Add files via upload

--- task298.js
+++ task298.js
@@ -12,23 +12,58 @@ switchs.onclick = function(){
 
 
 
+
 var audios = [];
-for(var j =0;j<4;j++){
-	audios[j] = document.getElementById("audio"+j);
-	audios.push(audios[j]);
+
+for(var j =1;j<5;j++){
+var a = document.getElementById("audio"+j);
+audios.push(a);
 }
 
-for(var i =1;i<5;i++){
-	$("#sx"+i).click(function(){
-		console.log(audios);
-		event.stopPropagation();
+$("#sx1").click(function(){
+	event.stopPropagation();
+	
+	if(audios[0].paused){
 		
-		if(audios[i-1].paused){
-			
-			audios[i-1].play();
-		}else{
-			audios[i-1].pause();
-		}
-	})
-}
+		audios[0].play();
+		return;
+	}else{
+		audios[0].pause();
+	}
+});
 
+$("#sx2").click(function(){
+	event.stopPropagation();
+	
+	if(audios[1].paused){
+		
+		audios[1].play();
+		return;
+	}else{
+		audios[1].pause();
+	}
+});
+
+$("#sx3").click(function(){
+	event.stopPropagation();
+	
+	if(audios[2].paused){
+		
+		audios[2].play();
+		return;
+	}else{
+		audios[2].pause();
+	}
+});
+
+$("#sx4").click(function(){
+	event.stopPropagation();
+	
+	if(audios[3].paused){
+		
+		audios[3].play();
+		return;
+	}else{
+		audios[3].pause();
+	}
+});
