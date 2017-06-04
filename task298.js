01fc884a46483b127116801be51fcd3922d879e3
蒋怡 <chcifung@gmail.com>
2017-06-04 18:14:48 +0800

    Add files via upload

--- task298.js
+++ task298.js
@@ -9,3 +9,26 @@ switchs.onclick = function(){
 	console.log("b");
 	}	
 }
+
+
+
+var audios = [];
+for(var j =0;j<4;j++){
+	audios[j] = document.getElementById("audio"+j);
+	audios.push(audios[j]);
+}
+
+for(var i =1;i<5;i++){
+	$("#sx"+i).click(function(){
+		console.log(audios);
+		event.stopPropagation();
+		
+		if(audios[i-1].paused){
+			
+			audios[i-1].play();
+		}else{
+			audios[i-1].pause();
+		}
+	})
+}
+
